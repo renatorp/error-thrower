03cf80fcba8e2e0fa2fdaedc59154b2ba908e8df
Renato Ribeiro <r3nato.rp+github@gmail.com>
2020-11-02 15:36:38 -0300

    Creating service to throw oom error

--- src/main/java/com/example/errorthrower/resource/ErrorResource.java
+++ src/main/java/com/example/errorthrower/resource/ErrorResource.java
@@ -4,17 +4,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 @RestController
 @RequestMapping("errors")
 public class ErrorResource {
 
 	@GetMapping("overflow")
-	public String throwNullPointer() {
+	public String throwStackOverflow() {
 		return recursive("teste");
 	}
 
 	private String recursive(String string) {
 		return recursive(string + string);
+	}
+
+	@GetMapping("oom")
+	public String throwOutOfMemory() {
+
+		Set<String> set = new HashSet<>();
+		String str = "";
+		while (str != "end") {
+			str += "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv";
+			set.add(str);
+		}
+
+		return "ok";
 	}
 
 }
